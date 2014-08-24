293e9dfcabdc3817cfb0db72179e92235745ca2b
Teny Zh <zh.teny.1@gmail.com>
2014-08-25 05:55:45 +0800

    complated Task:Protocol::ColumnParser

--- src/me/xcoding/opencdc/mysql/protocol/column/BinaryProtocolValue.java
+++ src/me/xcoding/opencdc/mysql/protocol/column/BinaryProtocolValue.java
@@ -1,10 +1,16 @@
 package me.xcoding.opencdc.mysql.protocol.column;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+import me.xcoding.opencdc.mysql.ColumnType;
+import me.xcoding.opencdc.mysql.EventType;
 import me.xcoding.opencdc.mysql.protocol.BasicReader;
 
 /**
  * 
  * @author Teny Zh(zh.Teny.1@gmail.com)
+ * @see http://www.mysqlsystems.com/mysql_source_analytics/html/d3/d06/classTable__map__log__event.html
  *
  */
 public interface BinaryProtocolValue {
@@ -16,8 +22,17 @@ interface ValueParser {
 } 
 
 class MDecimal implements ValueParser {
+	private static final int DECIMAL_BINARY_SIZE[] = {0, 1, 1, 2, 2, 3, 3, 4, 4, 4};
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
+		// A 1 byte unsigned int representing the precision,
+		// followed by a 1 byte unsigned int representing the number of decimals.
+		
+		final int precision = length >> 0x08;
+		final int decimals = length &  0xFF;
+		
+		// FIXME
+		
 		return null;
 	}
 }
@@ -46,7 +61,6 @@ class MLong implements ValueParser {
 class MFloat implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
-		// FIXME 
 		return Float.intBitsToFloat(reader.readFixedIntT4());
 	}
 }
@@ -69,7 +83,7 @@ class MNull implements ValueParser {
 class MTimestamp implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
-		return null;
+		return new Timestamp((reader.readFixedIntT4() & BasicReader._8F )* 1000);
 	}
 }
 
@@ -90,28 +104,61 @@ class MInt24 implements ValueParser {
 class MDate implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
-		return null;
+		// @Refer to Open Replicator & tungsten replicator
+		int value = reader.readFixedIntT3();
+
+		final int d = value % 32; value >>>= 5; 
+		final int m = value % 16;
+		final int y = value >> 4;
+		final Calendar cal = Calendar.getInstance();
+        cal.clear();
+        cal.set(y, m - 1, d);
+		return new java.sql.Date(cal.getTimeInMillis());
 	}
 }
 
 class MTime implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
-		return null;
+		// @refer to Open Replicator & tungsten replicator
+		int time = reader.readFixedIntT3();
+		
+		final int s = (int)(time % 100); time /= 100;
+		final int m = (int)(time % 100);
+		final int h = (int)(time / 100);
+		final Calendar c = Calendar.getInstance();
+        c.set(1970, 0, 1, h, m, s);
+        c.set(Calendar.MILLISECOND, 0);
+        
+        return new java.sql.Time(c.getTimeInMillis());
 	}
 }
 
 class MDateTime implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
-		return null;
+		// @refer to Open Replicator & tungsten replicator
+		long value = reader.readFixedIntT8();
+		
+		final int second = (int)(value % 100); value /= 100;
+		final int minute = (int)(value % 100); value /= 100;
+		final int hour = (int)(value % 100); value /= 100;
+		final int day = (int)(value % 100); value /= 100;
+		final int month = (int)(value % 100);
+		final int year = (int)(value / 100);
+		
+		final Calendar c = Calendar.getInstance();
+        c.set(year, month - 1, day, hour, minute, second);
+        c.set(Calendar.MILLISECOND, 0);
+        
+        return c.getTime();
 	}
 }
 
 class MYear implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
-		return null;
+		return 1900 + reader.readFixedIntT1();
 	}
 }
 
@@ -126,7 +173,6 @@ class MVarchar implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
 		int v = length > 0x0FF ? reader.readFixedIntT2() : reader.readFixedIntT1();
-		System.out.println(v);
 		return reader.readStringVarLen(v);
 	}
 } 
@@ -134,38 +180,72 @@ class MVarchar implements ValueParser {
 class MBit implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
+		
 		return null;
 	}
 }
 
 class MTimestamp2 implements ValueParser {
-	@Override
+	@Override 
 	public Object valueOf(int length, BasicReader reader) {
-		// TODO Auto-generated method stub
-		return null;
+		final long time = reader.readVarLenLongU(4);
+		final int nanos = reader.readVarLenIntU((length + 1) /2);
+
+		Timestamp ts = new Timestamp(time);
+		ts.setNanos(nanos);
+		
+		return ts;
 	}
 }
 
-class MDataTime2 implements ValueParser {
-	@Override
+class MDateTime2 implements ValueParser {
+	@Override 
 	public Object valueOf(int length, BasicReader reader) {
-		// TODO Auto-generated method stub
-		return null;
+		// @refer to Open Replicator & tungsten replicator
+		final long time = reader.readVarLenLongU(5);
+		final int nanos = reader.readVarLenIntU((length + 1) /2);
+
+		final long x = (time >> 22) & 0x1FFFFL;
+		final int year = (int)(x / 13);
+		final int month = (int)(x % 13);
+		final int day = ((int)(time >> 17)) & 0x1F;
+		final int hour = ((int)(time >> 12)) & 0x1F;
+		final int minute = ((int)(time >> 6)) & 0x3F;
+		final int second = ((int)(time >> 0)) & 0x3F;
+		
+		final Calendar c = Calendar.getInstance();
+        c.set(year, month - 1, day, hour, minute, second);
+        c.set(Calendar.MILLISECOND, 0);
+        final long millis = c.getTimeInMillis();
+       
+        return new java.util.Date(millis + (nanos / 1000000));
 	}
 }
 
 class MTime2 implements ValueParser {
-	@Override
+	@Override 
 	public Object valueOf(int length, BasicReader reader) {
-		// TODO Auto-generated method stub
-		return null;
+		// @refer to Open Replicator & tungsten replicator
+		final int time = reader.readVarLenIntU(3);
+		final int nanos = reader.readVarLenIntU((length + 1) / 2);
+		
+		final int h = (time >> 12) & 0x3FF;
+		final int m = (time >> 6) & 0x3F;
+		final int s = (time >> 0) & 0x3F;
+		
+		final Calendar c = Calendar.getInstance();
+        c.set(1970, 0, 1, h, m, s);
+        c.set(Calendar.MILLISECOND, 0);
+        final long millis = c.getTimeInMillis();
+        
+        return new java.sql.Time(millis + (nanos / 1000000));
 	}
 }
 
 class MNewDecimal implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
-		// TODO Auto-generated method stub
+		// TODO FIXME
 		return null;
 	}
 }
@@ -178,6 +258,7 @@ class MNewDecimal implements ValueParser {
 class MEnum implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
+		// FIXME DBSync 
 		throw new BinProtoValParserException();
 	}
 }
@@ -233,28 +314,73 @@ class MLongBlob implements ValueParser {
 class MBlob implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
-		// TODO Auto-generated method stub
-		return null;
+		int v = reader.readVarLenIntU(length);
+		
+		if(v < 0) 
+			throw new BinProtoValParserException("this length less than 0!");
+		
+		return reader.readBytesVarLen(v);
 	}
 }
 
 class MVarString implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
-		return null;
+		// FIXME fixed issue #66 ,binary类型在binlog中为var_string
+		int v = length > 256 ? (reader.readFixedIntT2()) : (reader.readFixedIntT2());
+		
+		return reader.readStringVarLen(v);
 	}
 }
 
 class MString implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
-		return null;
+		// fix #37426 
+		// @see http://bugs.mysql.com/bug.php?id=37426
+		int type = 0xFE;
+		
+		/*
+		 * 1 	byte0 : type
+		 * 2	byte1 : length
+		 */
+		final int meta0 = length & 0xFF;
+		final int meta1 = (length >> 8 ) & 0xFF;
+		
+		if(meta1 > 0) {
+			if ((meta0 & 0x30) != 0x30) { // a long CHAR() field: see #37426
+				type = meta0 | 0x30;
+				length = meta1 | (((meta0 & 0x30) ^ 0x30) << 4); 
+			} else {
+				switch (meta0) {
+				case ColumnType.MYSQL_TYPE_SET:
+				case ColumnType.MYSQL_TYPE_ENUM: {
+					type = meta0;
+					length = meta1;
+					break;
+				}
+				case ColumnType.MYSQL_TYPE_STRING: {
+					if(length > 256) { // FIXME
+						throw new BinProtoValParserException("length greater than 256!");
+					}
+					length = length > 256 ? reader.readFixedIntT2() : reader.readFixedIntT1();
+					
+					return reader.readStringVarLen(length);
+				}
+				default:
+					throw new RuntimeException("assertion failed, unknown column type: " + type);
+				}
+			}
+		}
+		
+		return ColumnValueParser.valueOf(type, (meta0 << 8) | meta1, reader);
 	}
 }
 
 class MGeometry implements ValueParser {
 	@Override
 	public Object valueOf(int length, BasicReader reader) {
+		// FIXME TODO
 		return null;
 	}
 }
